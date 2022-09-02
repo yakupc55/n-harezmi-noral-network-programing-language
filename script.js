3c69f095096b643241337a7db8f1e995fbca1b2f
Diyar Ceran <diyar.ceran@devoks.com.tr>
2022-09-02 16:17:14 +0200

    calculate circles' cordinates draw circles

--- script.js
+++ script.js
@@ -11,7 +11,45 @@ var networkSize=2;
 var networkListSize=(networkSize*2)+1;
 var networkList=[];
 
+
+var distanceX = 150;
+var distanceY = 100;
+var circleSize = 50;
+
 function createNetwork(){
+    createNetworkFirstList();
+    calculateCirclesCordinates();
+    drawNetworkCircles();
+}
+
+function calculateCirclesCordinates()
+{
+    console.log("calculateCircle");
+    for (let y = 0; y < networkList.length; y++) {
+        let firtDistance = (networkList[y].length - 1)*(distanceX/2);
+        let startX= startNetworkX-firtDistance;
+    
+        for (let x = 0; x < networkList[y].length; x++) {
+            networkList[y][x].y =startNetworkY +( distanceY * y);
+            networkList[y][x].x =startX+( distanceX * x);
+        }
+    }
+    
+    console.log("networkList");
+    console.log(networkList);
+}
+
+function drawNetworkCircles(){
+    for (let y = 0; y < networkList.length; y++) {
+        for (let x = 0; x < networkList[y].length; x++) {
+            ctx.beginPath();
+            ctx.arc(networkList[y][x].x, networkList[y][x].y, circleSize, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+    }
+}
+
+function createNetworkFirstList(){
 networkList[0]=([{x:startNetworkX,y:startNetworkY}]);
 
 var sizeCounter=networkSize;
