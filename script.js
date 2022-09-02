fbb52e6c36ccd6543df1eefa969bdc19ab461645
Diyar Ceran <diyar.ceran@devoks.com.tr>
2022-09-02 10:10:50 +0200

    first canvas test

---
+++ script.js
@@ -0,0 +1,10 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+function ciz() {
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+}
+
+console.log("çalıştı 2");
