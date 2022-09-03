ab36fba22a3da1bc3d3f86bc3871c968e6607af5
Diyar Ceran <diyar.ceran@devoks.com.tr>
2022-09-03 08:57:49 +0200

    give index to norons. and first steps of check list

--- script.js
+++ script.js
@@ -3,40 +3,89 @@ var ctx = c.getContext("2d");
 var alanW=c.width;
 var alanH=c.height;
 
-var ortaX=alanW/2;
-var ortaY=alanH/2;
-var startNetworkX=ortaX;
-var startNetworkY=50;
+var centerX=alanW/2;
+var centerY=alanH/2;
+var startNetworkX=120;
+var startNetworkY=centerY;
 var networkSize=2;
 var networkListSize=(networkSize*2)+1;
 var networkList=[];
+var NoronSize=0;
 
-
-var distanceX = 150;
-var distanceY = 100;
-var circleSize = 50;
+var distanceX = 160;
+var distanceY = 160;
+var circleSize = 60;
 
 function createNetwork(){
     createNetworkFirstList();
     calculateCirclesCordinates();
     drawNetworkCircles();
+    giveIndexs();
+    givePathsToNorons();
 }
 
 function calculateCirclesCordinates()
 {
-    console.log("calculateCircle");
-    for (let y = 0; y < networkList.length; y++) {
-        let firtDistance = (networkList[y].length - 1)*(distanceX/2);
-        let startX= startNetworkX-firtDistance;
+    //console.log("calculateCircle");
+    for (let i = 0; i < networkList.length; i++) {
+        let firtDistance = (networkList[i].length - 1)*(distanceY/2);
+        let startY= startNetworkY-firtDistance;
     
-        for (let x = 0; x < networkList[y].length; x++) {
-            networkList[y][x].y =startNetworkY +( distanceY * y);
-            networkList[y][x].x =startX+( distanceX * x);
+        for (let j = 0; j < networkList[i].length; j++) {
+            networkList[i][j].y =startY +( distanceY * j);
+            networkList[i][j].x =startNetworkX+( distanceX * i);
         }
     }
     
-    console.log("networkList");
-    console.log(networkList);
+    // console.log("networkList");
+    // console.log(networkList);
+}
+
+function isExistInIndex(i,j){
+    console.log("i : "+i+" j : "+j);
+    if(typeof networkList[i] === 'undefined') {
+        return false;
+    }
+    if(typeof networkList[i][j] === 'undefined') {
+        return false;
+    }
+    
+        return true;
+}
+
+function giveIndexs(){
+    // console.log("give indexs");
+    var counter=0;
+    for (let i = 0; i < networkList.length; i++) {
+        for (let j = 0; j < networkList[i].length; j++) {
+            networkList[i][j].index=counter;
+            counter++;
+        }
+    }
+    NoronSize=counter;
+    // console.log("networkList");
+    // console.log(networkList);
+}
+
+function addPathstoANoron(i,j){
+    //before control
+    if(isExistInIndex(i-1,0))
+    {
+        console.log("it has a previous value");
+    }
+    // later control
+    if(isExistInIndex(i+1,0)){
+        console.log("it has a next value");
+    }
+}
+
+function givePathsToNorons(){
+    for (let i = 0; i < networkList.length; i++) {
+        for (let j = 0; j < networkList[i].length; j++) {
+            networkList[i][j].paths= [];
+            addPathstoANoron(i,j);
+        }
+    }
 }
 
 function drawNetworkCircles(){
@@ -78,8 +127,8 @@ function ciz() {
 // öncelikle tüm alanı temizliyoruz
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, alanW, alanH);  
-ctx.moveTo(ortaX, startNetworkY);
-ctx.lineTo(ortaX, startNetworkY+25);
+ctx.moveTo(centerX, startNetworkY);
+ctx.lineTo(centerX, startNetworkY+25);
 ctx.stroke();
 createNetwork();
 }
